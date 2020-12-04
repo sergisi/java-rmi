c7bb922f6abe3fc10bc8e5c12c431fd55128059f
quimpm <quimpm99@gmail.com>
2020-12-04 18:50:38 +0100

    added Question

--- Server/src/main/java/Question.java
+++ Server/src/main/java/Question.java
@@ -1,4 +1,11 @@
-public class Question {
+import Exceptions.BadQuestionExceptions;
+
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public final class Question {
 
     private String question;
     private Integer correctAnswer;
@@ -16,8 +23,41 @@ public class Question {
         return question;
     }
 
-    public static Question parseLine(String question){
-        return new Question("Hola", 2);
+    //Question?;choice1;choice2;choice3;...;correct_answer_number.
+    public static Question parseLine(String question) throws BadQuestionExceptions {
+        String [] splitedQuestion = question.split(";");
+        if(splitedQuestion.length < 4){
+            throw new BadQuestionExceptions("Not a valid Question");
+        }
+        String questionStatement = splitedQuestion[0];
+        Integer correct_answer = Integer.parseInt(splitedQuestion[splitedQuestion.length-1]);
+        String[] answers = IntStream.range(1, splitedQuestion.length)
+                .mapToObj(i -> splitedQuestion[i])
+                .toArray(String[]::new);
+        String parsedQuestion = parseQuestion(questionStatement, answers);
+        return new Question(parsedQuestion, correct_answer);
     }
 
+    private static String parseQuestion(String questionStatement, String[] answers){
+        String parsedQuestion = questionStatement+"\n";
+        for (Integer i = 0; i < answers.length; i++) {
+            i++;
+            parsedQuestion += "- ["+ i.toString() + "]: "+ answers[i]+"\n";
+            i--;
+        }
+        return parsedQuestion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question1 = (Question) o;
+        return Objects.equals(question, question1.question) && Objects.equals(correctAnswer, question1.correctAnswer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(question, correctAnswer);
+    }
 }
