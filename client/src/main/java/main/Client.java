031ac2388296d3e979f8499d8df23aed57a5ece5
Sergi Simon Balcells <sergisiba@gmail.com>
2020-12-09 15:34:17 +0100

    Removed unused import.

--- client/src/main/java/main/Client.java
+++ client/src/main/java/main/Client.java
@@ -8,7 +8,6 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
 public class Client {
 
